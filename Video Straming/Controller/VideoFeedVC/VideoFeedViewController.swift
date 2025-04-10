ef96b48c8b0ee5f469bb7fad974e2e183811a441
Md. Tipu <tipu.diu.cse@gmail.com>
2025-04-10 17:01:34 +0600

    update of code cleaning
    update of code cleaning

--- Video Straming/Controller/VideoFeedVC/VideoFeedViewController.swift
+++ Video Straming/Controller/VideoFeedVC/VideoFeedViewController.swift
@@ -9,13 +9,13 @@ import UIKit
 import AVKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
-
+    
     private var videos: [Video] = []
     
     var currentlyPlayingCell: MediaCell?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
@@ -24,9 +24,9 @@ class ViewController: UIViewController {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
             self.playVisibleVideo()
         }
-
+        
     }
-
+    
     private func setupTableView() {
         tableView.dataSource = self
         tableView.delegate = self
@@ -34,26 +34,26 @@ class ViewController: UIViewController {
         tableView.separatorStyle = .none
         tableView.showsVerticalScrollIndicator = false
     }
-
-//    private func loadSampleVideos() {
-//        let sampleURLs = [
-//            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-//            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-//            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-//            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-//            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-//            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-//            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
-//            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
-//        ]
-//
-//        videos = sampleURLs.compactMap {
-//            guard let url = URL(string: $0) else { return nil }
-//            return Video(title: "Sample Video", videoURL: url)
-//        }
-//
-//        tableView.reloadData()
-//    }
+    
+    //    private func loadSampleVideos() {
+    //        let sampleURLs = [
+    //            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    //            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    //            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    //            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    //            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    //            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    //            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    //            "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    //        ]
+    //
+    //        videos = sampleURLs.compactMap {
+    //            guard let url = URL(string: $0) else { return nil }
+    //            return Video(title: "Sample Video", videoURL: url)
+    //        }
+    //
+    //        tableView.reloadData()
+    //    }
     
     private func loadSampleVideos() {
         let videoService = PexelsVideoService()
@@ -63,60 +63,60 @@ class ViewController: UIViewController {
         }
     }
     
-
-
-
+    
+    
+    
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return videos.count
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
+        
         guard let cell = tableView.dequeueReusableCell(withIdentifier: MediaCell.identifier, for: indexPath) as? MediaCell else {
             return UITableViewCell()
         }
         
         let video = videos[indexPath.row]
         cell.configure(with: video)
-
+        
         cell.onPlaybackTimeUpdate = { [weak self] time in
             self?.videos[indexPath.row].lastPlaybackTime = time
         }
-
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return UIScreen.main.bounds.height
     }
-
+    
 }
 
 extension ViewController: UITableViewDelegate {
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         playVisibleVideo()
     }
-
+    
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         if !decelerate {
             playVisibleVideo()
         }
     }
-
+    
     
     private func playVisibleVideo() {
         guard let visibleIndexPath = tableView.indexPathsForVisibleRows?.first,
               let cell = tableView.cellForRow(at: visibleIndexPath) as? MediaCell else { return }
-
+        
         // Stop previous cell
         if currentlyPlayingCell != cell {
             currentlyPlayingCell?.stopPlayback()
             currentlyPlayingCell = cell
         }
-
+        
         // Start new video
         cell.playVideo()
     }
--- Video Straming/Utilities/Cells/MediaCell/MediaCell.swift
+++ Video Straming/Utilities/Cells/MediaCell/MediaCell.swift
@@ -8,19 +8,18 @@
 import UIKit
 import AVFoundation
 
-
 class MediaCell: UITableViewCell {
     static let identifier = "MediaCell"
-
+    
     @IBOutlet weak var videoContainerView: UIView!
-
+    
     private var player: AVPlayer?
     private var playerLayer: AVPlayerLayer?
     
     private var timeObserverToken: Any?
     private var video: Video?
     var onPlaybackTimeUpdate: ((CMTime) -> Void)?
-
+    
     
     //other propirties
     private let backwardButton: UIButton = {
@@ -29,21 +28,21 @@ class MediaCell: UITableViewCell {
         button.titleLabel?.font = .systemFont(ofSize: 20)
         return button
     }()
-
+    
     private let forwardButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("⏩", for: .normal)
         button.titleLabel?.font = .systemFont(ofSize: 20)
         return button
     }()
-
+    
     private let progressSlider: UISlider = {
         let slider = UISlider()
         slider.minimumValue = 0
         slider.maximumValue = 1
         return slider
     }()
-
+    
     private let currentTimeLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 12)
@@ -51,7 +50,7 @@ class MediaCell: UITableViewCell {
         label.text = "00:00"
         return label
     }()
-
+    
     private let durationLabel: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 12)
@@ -64,7 +63,7 @@ class MediaCell: UITableViewCell {
         super.awakeFromNib()
         
         videoContainerView.backgroundColor = .black
-
+        
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         videoContainerView.addGestureRecognizer(tapGesture)
         
@@ -77,11 +76,11 @@ class MediaCell: UITableViewCell {
             $0.translatesAutoresizingMaskIntoConstraints = false
             videoContainerView.addSubview($0)
         }
-
+        
         backwardButton.addTarget(self, action: #selector(didTapBackward), for: .touchUpInside)
         forwardButton.addTarget(self, action: #selector(didTapForward), for: .touchUpInside)
         progressSlider.addTarget(self, action: #selector(sliderValueChanged(_:)), for: .valueChanged)
-
+        
         NSLayoutConstraint.activate([
             // Progress slider
             progressSlider.leadingAnchor.constraint(equalTo: videoContainerView.leadingAnchor, constant: 50),
@@ -105,122 +104,120 @@ class MediaCell: UITableViewCell {
             forwardButton.centerYAnchor.constraint(equalTo: progressSlider.centerYAnchor)
         ])
     }
-
+    
     @objc private func didTapBackward() {
         guard let player = player else { return }
         let currentTime = player.currentTime()
         let newTime = CMTimeSubtract(currentTime, CMTimeMake(value: 10, timescale: 1))
         player.seek(to: newTime)
     }
-
+    
     @objc private func didTapForward() {
         guard let player = player else { return }
         let currentTime = player.currentTime()
         let newTime = CMTimeAdd(currentTime, CMTimeMake(value: 10, timescale: 1))
         player.seek(to: newTime)
     }
-
+    
     @objc private func sliderValueChanged(_ sender: UISlider) {
         guard let duration = player?.currentItem?.duration else { return }
         let totalSeconds = CMTimeGetSeconds(duration)
         let seekTime = CMTime(seconds: Double(sender.value) * totalSeconds, preferredTimescale: 600)
         player?.seek(to: seekTime)
     }
-
+    
     
     private func formatTime(_ seconds: Double) -> String {
         let mins = Int(seconds) / 60
         let secs = Int(seconds) % 60
         return String(format: "%02d:%02d", mins, secs)
     }
-
-
+    
+    
     override func prepareForReuse() {
         super.prepareForReuse()
-
+        
         if let token = timeObserverToken {
             player?.removeTimeObserver(token)
             timeObserverToken = nil
         }
-
+        
         // Save playback time back to the video model
         if let currentTime = player?.currentTime() {
             onPlaybackTimeUpdate?(currentTime)
         }
-
-
+        
+        
         player?.pause()
         playerLayer?.removeFromSuperlayer()
         player = nil
     }
-
+    
     
     func configure(with video: Video) {
         self.video = video  // Save a reference for updating playback time later
-
+        
         player = AVPlayer(url: video.videoURL)
         player?.isMuted = true
-
+        
         playerLayer?.removeFromSuperlayer()
         playerLayer = AVPlayerLayer(player: player)
         playerLayer?.videoGravity = .resizeAspect
         playerLayer?.frame = videoContainerView.bounds
-
+        
         if let layer = playerLayer {
             videoContainerView.layer.insertSublayer(layer, at: 0)
         }
-
+        
         // Seek to saved time
         player?.seek(to: video.lastPlaybackTime)
-
+        
         addPeriodicTimeObserver()
-
+        
         // Set duration label
         if let duration = player?.currentItem?.asset.duration {
             let seconds = CMTimeGetSeconds(duration)
             durationLabel.text = formatTime(seconds)
         }
     }
-
-
     
     private func addPeriodicTimeObserver() {
         guard let player = player else { return }
-
+        
         // Remove previous observer if any
         if let token = timeObserverToken {
             player.removeTimeObserver(token)
             timeObserverToken = nil
         }
-
+        
         let interval = CMTime(seconds: 1, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
-
+        
         timeObserverToken = player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [weak self] time in
             guard let self = self,
                   let duration = player.currentItem?.duration else { return }
-
+            
             let currentSeconds = CMTimeGetSeconds(time)
             let durationSeconds = CMTimeGetSeconds(duration)
-
+            
             if durationSeconds.isFinite && durationSeconds > 0 {
                 self.progressSlider.value = Float(currentSeconds / durationSeconds)
                 self.currentTimeLabel.text = self.formatTime(currentSeconds)
             }
         }
     }
-
-
-
+    
+    
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         playerLayer?.frame = videoContainerView.bounds
     }
-
-
+    
+    
     func playVideo() {
         player?.play()
     }
-
+    
     func pauseVideo() {
         player?.pause()
     }
@@ -228,10 +225,10 @@ class MediaCell: UITableViewCell {
     func stopPlayback() {
         player?.pause()
     }
-
+    
     @objc private func handleTap() {
         guard let player = player else { return }
-
+        
         if player.timeControlStatus == .playing {
             player.pause()
         } else {
